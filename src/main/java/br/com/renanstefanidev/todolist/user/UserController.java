e6797704dcd1c0133c57919a5f375e9b2f3c6e9f
Renan Stefani <renanstefanir@gmail.com>
2023-10-09 10:44:51 -0300

    Criado nosso primeiro controller Criado o controller de user e configurada a requisição post do mesmo

---
+++ src/main/java/br/com/renanstefanidev/todolist/user/UserController.java
@@ -0,0 +1,38 @@
+package br.com.renanstefanidev.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificadores:
+ * public
+ * private
+ * protected
+ */
+
+@RestController
+// Define o tipo de controller no spring (@Controller ou @RestController)
+
+@RequestMapping("/user")
+// Define a rota do método
+
+public class UserController {
+    /**
+     * String (texto)
+     * Integer (int)
+     * Double (double) Números com casas decimais
+     * Float (float) Similar ao double, com diferença em precisão
+     * Char (A C) Caracteres
+     * Date (data)
+     * void () Quando o método não retorna algo
+     */
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        // @RequestBody aponta que UserModel está no body da requisição
+        System.out.println(userModel.name);
+    }
+    
+}
---
+++ src/main/java/br/com/renanstefanidev/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.renanstefanidev.todolist.user;
+
+public class UserModel {
+    
+    public String username;
+    public String name;
+    public String password;
+}
