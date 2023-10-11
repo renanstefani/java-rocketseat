dc508b36eea3934e6bb985c49cdb4a4f520e16a3
Renan Stefani <renanstefanir@gmail.com>
2023-10-11 16:38:15 -0300

    Implementada biblioteca Bcrypt Biblioteca implementada e criação de hash para password de novo usuário já configurada e funcional

--- src/main/java/br/com/renanstefanidev/todolist/user/UserController.java
+++ src/main/java/br/com/renanstefanidev/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.renanstefanidev.todolist.user;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Modificadores:
@@ -38,7 +41,13 @@ public class UserController {
             // Status Code
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        // Criando Hash para senha com Bcrypt Lib
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
         
+        userModel.setPassword(passwordHashred);
+
+        // User criado
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
