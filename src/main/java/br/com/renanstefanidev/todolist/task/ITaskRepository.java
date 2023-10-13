bfa5dfabbdb1ab0e979a537f08f898afa3d5a74b
Renan Stefani <renanstefanir@gmail.com>
2023-10-13 02:15:56 -0300

    Configurada request de listar tarefas do usuário Configurado método get para listagem das tasks cadastradas por usuário respeitando as validações

--- src/main/java/br/com/renanstefanidev/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/renanstefanidev/todolist/task/ITaskRepository.java
@@ -3,7 +3,11 @@ package br.com.renanstefanidev.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
     
+    // Resgatar Tasks somente do usuário
+    List<TaskModel> findByIdUser(UUID idUser);
 }
